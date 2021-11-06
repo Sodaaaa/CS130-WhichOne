2912ea9304d0671ee77e1d14e7fb52bfa220cb2c
Julia0524 <julia980524@outlook.com>
2021-11-06 13:39:27 -0700

    add antd

--- src/pages/question/question.js
+++ src/pages/question/question.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+import { Layout, Menu, Breadcrumb } from 'antd/lib';
+import { HomeOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+const { Header, Content, Footer } = Layout;
 
 export default class question extends Component {
   render() {
-    return <h1>Post a Question</h1>;
+    return (
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="home" icon={<HomeOutlined />}>
+              <Link to='/homepage'>Home</Link>
+            </Menu.Item>
+            <Menu.Item key="post" icon={<HomeOutlined />}>
+              <Link to='/question'>question</Link>
+            </Menu.Item>
+            <Menu.Item key="vote" icon={<HomeOutlined />}>
+              Vote
+            </Menu.Item>            
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+              <div className="site-layout-content">Content</div>
+        </Content>  
+      </Layout>    
+    )
+    // return <h1>Post a Question</h1>;
   }
 }
 
