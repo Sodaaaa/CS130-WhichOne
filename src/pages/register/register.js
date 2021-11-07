5f686416d85cbda91bfa424048e359e15cf3e59f
Julia0524 <julia980524@outlook.com>
2021-11-06 21:42:55 -0700

    add MenuBar component

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -15,29 +15,15 @@ import {
   AutoComplete,
 } from 'antd';
 import './register.css';
+import MenuBar from '../../components/MenuBar/MenuBar'
+
 const { Header, Content } = Layout;
 
 export default class login extends Component {
   render() {
     return (      
       <Layout className="layout">
-        <Header>
-          <div className="logo"> Which One </div>
-          <Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']}>
-            <Menu.Item key="home" icon={<HomeOutlined />}>
-              <Link to='/homepage'>Home</Link>
-            </Menu.Item>
-            <Menu.Item key="post" icon={<PlusCircleOutlined />}>
-              <Link to='/question'>Post</Link>
-            </Menu.Item>
-            <Menu.Item key="vote" icon={<BarsOutlined />}>
-              <Link to='/vote'>Vote</Link>
-            </Menu.Item>       
-            <Menu.Item key="user" icon={<UserOutlined />}>
-              <Link to='/login'>Login</Link>
-            </Menu.Item>        
-          </Menu>
-        </Header>
+        <MenuBar selected="login"></MenuBar>
         <Content style={{ padding: '50px 50px' }}>
               <div className="site-layout-content">
                 <Form
