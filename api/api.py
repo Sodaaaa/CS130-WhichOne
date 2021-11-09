8bd33b9844f96fbb20edd52d827d8a20d3940eb0
fangqingxu <xufq1999@hotmail.com>
2021-11-08 22:34:01 -0800

    add tables

--- api/api.py
+++ api/api.py
@@ -1,3 +1,4 @@
+from os import name
 import time
 import enum
 from flask import Flask
@@ -8,11 +9,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Questions(db.Model):
-    questionID = db.Column(db.Integer, primary_key=True)
-    ownerID = db.Column(db.Integer)
+class Question(db.Model):
+    QuestionID = db.Column(db.Integer, primary_key=True)
+    ownerID = db.Column(db.Integer, db.ForeignKey('user.UID'), nullable=False)
     time = db.Column(db.DateTime)
-    tag = db.Column(db.Enum)
+    tag = db.Column(db.string)
     question = db.Column(db.String(200))
     anonymous = db.Column(db.Boolean)
     options = db.Column(db.Text)
@@ -21,3 +22,16 @@ class Questions(db.Model):
     feedback = db.Column(db.Integer)
     isAutoSelect = db.Column(db.Boolean)
     timeLimit = db.Column(db.Integer)
+
+class Option(db.Model):
+    OptionID = db.Column(db.Integer, primary_key=True)
+    questionID = db.Column(db.Integer, db.ForeignKey('question.QuestionID'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(50))
+    votes = db.Column(db.Integer, nullable=False)
+
+class Feedback(db.Model):
+    FeedbackID = db.Column(db.Integer, primary_key=True)
+    questionID = db.Column(db.Integer, db.ForeignKey('question.QuestionID'), nullable=False)
+    text = db.Column(db.String(500))
+    image = db.Column(db.String(50))
