34ebbb92ce435224778b1fb96ef7e51942659f97
zikun-li <lizikunzk@gmail.com>
2021-11-10 19:00:59 -0800

    add constructors for data models

--- model.py
+++ model.py
@@ -10,16 +10,27 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '0bbabb38f22b256ab947284622266494'
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     UID = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     questions = db.relationship('Question', backref='author', lazy=True)
 
+    def __init__(self, username, email, password, image_file=None):
+        self.username = username
+        self.email = email
+        self.password = password
+
+        if image_file != None:
+            self.image_file = image_file
+
     def __repr__(self):
         return f"User('{self.UID}', '{self.username}', '{self.email}', '{self.image_file}')"
+
 
 class Question(db.Model):
     QuestionID = db.Column(db.Integer, primary_key=True)
@@ -31,28 +42,62 @@ class Question(db.Model):
     options = db.Column(db.Text)
     likes = db.Column(db.Integer)
     dislikes = db.Column(db.Integer)
-    feedback = db.Column(db.Integer)
+    feedbackID = db.Column(db.Integer)
     isAutoSelect = db.Column(db.Boolean)
     timeLimit = db.Column(db.Integer)
-    
+
+    def __init__(self, ownerID, time, tag, question, anonymous, options, isAutoSelect, timeLimit):
+        self.ownerID = ownerID
+        self.time = time
+        self.tag = tag
+        self.question = question
+        self.anonymous = anonymous
+        self.options = options
+        self.likes = 0
+        self.dislikes = 0
+        self.feedbackID = -1
+        self.isAutoSelect = isAutoSelect
+        self.timeLimit = timeLimit
+
     def __repr__(self):
         return f"Question('{self.QuestionID}', '{self.ownerID}', '{self.time}')"
 
+
 class Option(db.Model):
     OptionID = db.Column(db.Integer, primary_key=True)
-    questionID = db.Column(db.Integer, db.ForeignKey('question.QuestionID'), nullable=False)
+    questionID = db.Column(db.Integer, db.ForeignKey(
+        'question.QuestionID'), nullable=False)
     name = db.Column(db.String(100), nullable=False)
     image = db.Column(db.String(50))
     votes = db.Column(db.Integer, nullable=False)
 
+    def __init__(self, questionID, name, image=None):
+        self.questionID = questionID
+        self.name = name
+        if image != None:
+            self.image = None
+        else:
+            self.image = 'none'
+        self.votes = 0
+
     def __repr__(self):
         return f"Option('{self.OptionID}', '{self.questionID}', '{self.votes}')"
 
+
 class Feedback(db.Model):
     FeedbackID = db.Column(db.Integer, primary_key=True)
-    questionID = db.Column(db.Integer, db.ForeignKey('question.QuestionID'), nullable=False)
+    questionID = db.Column(db.Integer, db.ForeignKey(
+        'question.QuestionID'), nullable=False)
     text = db.Column(db.String(500))
     image = db.Column(db.String(50))
+
+    def __init__(self, questionID, text, image=None):
+        self.questionID = questionID
+        self.text = text
+        if image != None:
+            self.image = None
+        else:
+            self.image = 'none'
 
     def __repr__(self):
         return f"Feedback('{self.FeedbackID}', '{self.questionID}', '{self.text}')"
