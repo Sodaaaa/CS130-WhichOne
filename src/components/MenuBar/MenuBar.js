5cd5d085fd81adb76c4694542dcb1878020aac99
Julia0524 <julia980524@outlook.com>
2021-11-18 17:29:47 -0800

    update MenuBar and profile page

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
+import {Link, Redirect, withRouter} from 'react-router-dom'
 import {Layout, Menu} from 'antd/lib';
 import {HomeOutlined, PlusCircleOutlined, BarsOutlined, UserOutlined } from '@ant-design/icons';
 
 const { Header } = Layout;
 
 class MenuBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {loggedIn: false};
+    // this.props.location.state.loggedIn = false;
+  }
+
   render() {
+    // const loggedIn = this.state.loggedIn;
+    // this.forceUpdate();
+    let UserMenu;
+    if (localStorage.getItem('loggedIn')=="true") {
+      UserMenu = <Menu.Item key="profile" icon={<UserOutlined />}>
+                  <Link to='/profile'>Profile</Link>
+                </Menu.Item>;                
+    } else {
+      UserMenu = <Menu.Item key="login" icon={<UserOutlined />}>
+                  <Link to='/login'>Login</Link>
+                </Menu.Item>;
+    }
     return (
       <Header>
           <div className="logo"> Which One </div>
@@ -20,11 +38,9 @@ class MenuBar extends Component {
             <Menu.Item key="vote" icon={<BarsOutlined />}>
               <Link to='/vote'>Vote</Link>
             </Menu.Item>       
-            <Menu.Item key="login" icon={<UserOutlined />}>
-              <Link to='/login'>Login</Link>
-            </Menu.Item>        
+            {UserMenu}
           </Menu>
-        </Header>   
+        </Header>        
     )
   }
 }
--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -5,15 +5,12 @@ import MenuBar from '../../components/MenuBar/MenuBar'
 
 const { Header, Content } = Layout;
 
-export default class vote extends Component {
+export default class profile extends Component {
   render() {
     return (
       <Layout className="layout">
-        <Header>
-          <div className="logo"> Which One </div>
-          <MenuBar selected="login"></MenuBar>
-        </Header>
-        <Content style={{ padding: '0px 50px' }} className='profile-content'>
+        <MenuBar selected="profile"></MenuBar>
+        <Content style={{ padding: '50px 50px' }} className='profile-content'>
           Profile
         </Content>
       </Layout>
