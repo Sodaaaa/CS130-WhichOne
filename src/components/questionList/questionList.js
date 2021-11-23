0f4a6c1917d45ff768503f4760361576e9257d59
unknown <l2545415085@hotmail.com>
2021-11-23 14:55:08 -0800

    likes and dislikes

--- src/components/questionList/questionList.js
+++ src/components/questionList/questionList.js
@@ -1,7 +1,7 @@
 
-import React, { Component } from 'react';
+import React, { Component, useState, createElement } from 'react';
 import OptionList from '../../components/optionList/OptionList';
-import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
+import { MessageOutlined, DislikeOutlined, LikeOutlined, DislikeFilled, LikeFilled, StarOutlined } from '@ant-design/icons';
 import { List, Avatar, Space} from 'antd';
 
 const listData = [];
@@ -24,7 +24,48 @@ const IconText = ({ icon, text }) => (
 );
 
 class QuestionList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {liked: false, disliked: false, likes:0, dislikes: 0};
+  }
+
+  like = () => {
+    if (this.state.liked == false) {
+      const {likes} = this.state;
+      this.setState({disliked: false, likes: likes + 1, liked: true});
+    } else {
+      const {likes} = this.state;
+      this.setState({likes: likes - 1, liked: false});
+    }
+  };
+  
+  dislike = () => {
+    if (this.state.disliked == false) {
+      const {dislikes} = this.state;
+      this.setState({liked: false, dislikes: dislikes + 1, disliked: true});
+    } else {
+      const {dislikes} = this.state;
+      this.setState({dislikes: dislikes - 1, disliked: false});
+    }
+  };
+
   render() {
+    let LikeAttitude;
+    console.log(this.state.liked)
+    if (this.state.liked == false) {
+      LikeAttitude = <LikeOutlined onClick={this.like}> {this.state.likes} </LikeOutlined>;
+    } else {
+      LikeAttitude = <LikeFilled onClick={this.like}> {this.state.likes} </LikeFilled>;
+    };
+
+    let DislikeAttitude;
+    console.log(this.state.disliked, this.state.dislikes)
+    if (this.state.disliked == false) {
+      DislikeAttitude = <DislikeOutlined onClick={this.dislike}> {this.state.dislikes} </DislikeOutlined>;
+    } else {
+      DislikeAttitude = <DislikeFilled onClick={this.dislike}> {this.state.dislikes} </DislikeFilled>;
+    };
+  
     return (
       <List
         itemLayout="vertical"
@@ -37,7 +78,7 @@ class QuestionList extends Component {
             key={item.title}
             actions={[
               <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-              <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+              // <IconText onClick={like} icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
               <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
             ]}
                         // extra={
@@ -48,6 +89,8 @@ class QuestionList extends Component {
                         //   />
                         // }
           >
+          {LikeAttitude}
+          {DislikeAttitude}
           <List.Item.Meta
             avatar={<Avatar src={item.avatar} />}
             title={item.title}
