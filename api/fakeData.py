c750f92ef13c76968bdc2fdcaadf029048a79066
fangqingxu <xufq1999@hotmail.com>
2021-11-22 18:06:27 -0800

    add fakeData

--- api/fakeData.py
+++ api/fakeData.py
@@ -1,4 +1,4 @@
-from api import db, User, Question
+from api import db, User, Question, Option
 import datetime
 
 db.drop_all()
@@ -10,8 +10,15 @@ user_data = [
 ]
 
 question_data = [
-    {'ownerId': 1, 'tag': 'Food', 'question': 'What food should I choose?', 'anonymous': False},
-    {'ownerId': 2, 'tag': 'Movie', 'question': 'What movie should I choose?', 'anonymous': False}
+    {'ownerId': 1, 'time': datetime.datetime(2021, 11, 20), 'tag': 'Food', 'question': 'What food should I choose?', 'anonymous': False, 'timeLimit': datetime.datetime(2021, 11, 30)},
+    {'ownerId': 2, 'time': datetime.datetime(2021, 11, 20), 'tag': 'Movie', 'question': 'What movie should I choose?', 'anonymous': False, 'timeLimit': datetime.datetime(2021, 11, 30)}
+]
+
+option_data = [
+    {'name': 'Food1', 'questionID': 1},
+    {'name': 'Food2', 'questionID': 1},
+    {'name': 'Movie1', 'questionID': 2},
+    {'name': 'Movie2', 'questionID': 2}
 ]
 
 for data in user_data:
@@ -19,7 +26,11 @@ for data in user_data:
     db.session.add(user)
 
 for data in question_data:
-    question = Question(data['ownerId'], datetime.datetime(2021, 11, 20), data['tag'], data['question'], data['anonymous'], datetime.datetime(2021, 11, 30))
+    question = Question(data['ownerId'], data['time'], data['tag'], data['question'], data['anonymous'], data['timeLimit'])
     db.session.add(question)
+
+for data in option_data:
+    question = Question.query.get(data['questionID'])
+    question.option.append(Option(data['name']))
 
 db.session.commit()
