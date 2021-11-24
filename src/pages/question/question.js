62d65e7474da5a3dd3e8ee0db2264a26534d4588
Jingwei Huang <j284huan@uwaterloo.ca>
2021-11-23 23:09:07 -0800

    added lucky draw

--- src/pages/question/question.js
+++ src/pages/question/question.js
@@ -4,6 +4,7 @@ import { UploadOutlined, MinusCircleOutlined, PlusOutlined } from '@ant-design/i
 import "./question.css"
 import MenuBar from '../../components/MenuBar/MenuBar'
 import axios from 'axios';
+import { LuckyWheel } from '@lucky-canvas/react'
 
 const { Content } = Layout;
 const normFile = (e) => {
@@ -17,7 +18,38 @@ const normFile = (e) => {
 };
 
 export default class question extends Component {
-  state = {visible: false, modalText: "", modalTopic: ""};
+  //state = {visible: false, modalText: "", modalTopic: ""};
+  constructor () {
+    super();
+    this.myLucky = React.createRef()
+    this.state = {
+      visible: false, modalText: "", modalTopic: "",
+      blocks: [
+        { padding: '13px', background: '#d64737' }
+      ],
+      prizes: [
+        { title: '1元红包', background: '#f9e3bb', fonts: [{ text: '1元红包', top: '18%' }] },
+        { title: '100元红包', background: '#f8d384', fonts: [{ text: '100元红包', top: '18%' }] },
+        { title: '0.5元红包', background: '#f9e3bb', fonts: [{ text: '0.5元红包', top: '18%' }] },
+        { title: '2元红包', background: '#f8d384', fonts: [{ text: '2元红包', top: '18%' }] },
+        { title: '10元红包', background: '#f9e3bb', fonts: [{ text: '10元红包', top: '18%' }] },
+        { title: '50元红包', background: '#f8d384', fonts: [{ text: '50元红包', top: '18%' }] },
+      ],
+      buttons: [
+        { radius: '50px', background: '#d64737' },
+        { radius: '45px', background: '#fff' },
+        { radius: '41px', background: '#f6c66f', pointer: true },
+        {
+          radius: '35px', background: '#ffdea0',
+          fonts: [{ text: '开始\n抽奖', fontSize: '18px', top: -18 }]
+        }
+      ],
+      defaultStyle: {
+        fontColor: '#d64737',
+        fontSize: '14px'
+      },
+    }
+  }
   
   onSubmit = (values) =>  {
     // console.log(values);
@@ -223,6 +255,30 @@ export default class question extends Component {
                     <p>{this.state.modalTopic}</p>
                     <p>{this.state.modalText}</p>
                     {/* <div style={{textAlign: 'center'}}><Spin/></div> */}
+                    <LuckyWheel
+                      ref={this.myLucky}
+                      width="300px"
+                      height="300px"
+                      blocks={this.state.blocks}
+                      prizes={this.state.prizes}
+                      buttons={this.state.buttons}
+                      defaultStyle={this.state.defaultStyle}
+                      onStart={() => { // 点击抽奖按钮会触发star回调
+                        // 调用抽奖组件的play方法开始游戏
+                        this.myLucky.current.play()
+                        // 模拟调用接口异步抽奖
+                        setTimeout(() => {
+                          // 假设拿到后端返回的中奖索引
+                          const index = Math.random() * 6 >> 0
+                          // 调用stop停止旋转并传递中奖索引
+                          this.myLucky.current.stop(index)
+                        }, 2500)
+                      }}
+                      onEnd={prize => { // 抽奖结束会触发end回调
+                        console.log(prize)
+                        alert('恭喜获得大奖:' + prize.title)
+                      }}
+                    ></LuckyWheel>
                   </Modal>
                   <Button type="primary" htmlType="submit"
                     className="question-post-btn">
